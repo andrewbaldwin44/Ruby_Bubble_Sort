d48fdc623e880ed87d37c593b18954183ae47ca7
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-05-08 16:56:07 -0400

    Improved method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,12 @@
 class Sort
-  def self.bubble_sort(array, &block)
-    while true
-      #equals half the array length when all is sorted
-      #comparing two items at a time -> half array length
-      sum_of_sorted_items = 0
+  def self.bubble_sort(list, &block)
+    sorted = []
 
-      array.each_cons(2) do |e, i|
-        e_index = array.index(e)
-        i_index = array.index(i)
+    #empty list when all elements have been moved to sorted
+    until list.empty?
+      list.each_cons(2) do |e, i|
+        e_index = list.index(e)
+        i_index = list.index(i)
 
         if block
            next if yield(e, i).negative?
@@ -15,19 +14,23 @@ class Sort
           next if e < i
         end
 
-        array[e_index], array[i_index] = array[i_index], array[e_index]
-
-        sum_of_sorted_items += 1
+        list[e_index], list[i_index] = list[i_index], list[e_index]
       end
-      break if sum_of_sorted_items == (array.length - 1) / 2
+
+      sorted.unshift(list.pop) #item bubbled to end is always the largest
     end
-    array
+    sorted
   end
 
-  def self.bubble_sort_by(array, &block)
-    bubble_sort(array, &block)
+  def self.bubble_sort_by(list, &block)
+    bubble_sort(list, &block)
   end
 end
 
 p Sort.bubble_sort([2, 5, 1, 20, 15, 7])
-p Sort.bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}
+p Sort.bubble_sort(["apples", "oranges", "xyplophone", "zebra", "cats"])
+
+large_array = (0..999).to_a.sample(99)
+p Sort.bubble_sort(large_array)
+
+p Sort.bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length}
